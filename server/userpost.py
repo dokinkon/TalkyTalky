1c0bb9bacac3e76ee48b7cf3db577a66ba12ebc1
FLCL <flcl46@gmail.com>
2011-10-14 00:14:53 +0800

    modify class

--- server/userpost.py
+++ server/userpost.py
@@ -3,12 +3,24 @@ from google.appengine.ext import db
 
 
 class UserPost(db.Model):
+    #Has one to many relation?
 
-    # UserID from Facebook
-    userId = db.StringProperty(required=True)
+    owner = db.ReferenceProperty(reference_class=TalkyUser, collection_name=posts)
+    dateTime = db.DateTimeProperty(auto_now_add=True)
+    content = db.StringProperty(multiline=True) 
+    spot = db.ReferenceProperty(reference_class=Spot, collection_name=posts)
 
-    date = db.DateTimeProperty(auto_now_add=True)
-    content = db.StringProperty(multiline=True)
-    spotName = db.StringProperty(required=True)
-    #replies  = db.ListProperty(db.Key)
+    image = db.BlobProperty()
+   
+    imageDesciption = db.StringProperty()
 
+    goods = db.IntegerProperty()
+
+    bads = db.IntegerProperty()
+
+    anonymous = db.BooleanProperty()
+
+    """
+    implicity properties
+    replies:<collection of PostReply>
+    """
